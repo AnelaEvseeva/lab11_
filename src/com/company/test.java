dab3fa54b6f199ae4b20dd4e0af00ca6cf00f5cd
AnelaEvseeva <s.flake02@mail.ru>
2021-12-27 21:26:09 +0900

    Initial commit

---
+++ src/com/company/test.java
@@ -0,0 +1,18 @@
+package com.company;
+public class test {
+    public static void main(String[] argc) {
+        Triangle tr = new Triangle(3, 4, 5);
+        tr.print();
+        tr.perimeter();
+        tr.area();
+        Rectangle rc = new Rectangle(3, 4);
+        rc.print();
+        rc.perimeter();
+        rc.area();
+        Array arr = new Array(4);
+        arr.enter();
+        arr.print();
+        arr.sort();
+        arr.print();
+    }
+}
